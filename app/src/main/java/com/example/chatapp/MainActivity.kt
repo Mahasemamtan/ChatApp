e998ee874d25bac4d02ce2f50a8866337e05d24c
Bojan Antic <maatori90@gmail.com>
2019-05-02 02:03:16 +0200

    -- added photo upload
    -- added storing user into firebase database
    -- added circular imageView

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.chatapp
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "RegistrationScreen"
+    private var  selectedPhotoURI: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         upload_image_button_register.setOnClickListener {
-            uploadImage()
+            getPhotoAndUpload()
         }
 
         login_textview_register.setOnClickListener {
@@ -36,7 +44,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun performRegistration() {
 
-        val username = username_textview_register.text.toString()
         val email = email_edittext_register.text.toString()
         val password = password_edittext_register.text.toString()
 
@@ -57,11 +64,70 @@ class MainActivity : AppCompatActivity() {
             .addOnFailureListener {
                 Toast.makeText(this, "${it.message}", Toast.LENGTH_LONG).show()
             }
+
+        uploadImageToFirebase()
     }
 
-    fun uploadImage() {
-        //TODO: Well, implement it, dooohhhh!!
-        Toast.makeText(this, "Do it!!", Toast.LENGTH_SHORT).show()
+    private fun uploadImageToFirebase() {
 
+        if (selectedPhotoURI == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoURI!!)
+            .addOnSuccessListener {
+                Log.d(TAG, "Photo uploaded: ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d(TAG, "Uploaded image location: $it")
+
+                    saveUserToFirebaseDatabase(it.toString())
+                }
+            }
+            .addOnFailureListener {
+                Log.e(TAG, it.message)
+            }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref =  FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, username_textview_register.text.toString(), profileImageUrl)
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d(TAG, "User saved to Firebase database.")
+            }
+            .addOnFailureListener {
+                Log.e(TAG, it.message)
+            }
+    }
+
+    fun getPhotoAndUpload() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/+"
+
+        startActivityForResult(intent, 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            // Return image and insert it into imageView
+            Log.d(TAG, "Photo was selected.")
+
+            selectedPhotoURI = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoURI)
+
+            upload_image_circle_image_view_register.setImageBitmap(bitmap)
+            upload_image_button_register.alpha = 0f
+//            val bitmapDrwable = BitmapDrawable(bitmap)
+//            upload_image_button_register.setBackgroundDrawable(bitmapDrwable)
+        }
     }
 }
+
+class User(val uid: String, val username: String, val profileImageUrl: String)
