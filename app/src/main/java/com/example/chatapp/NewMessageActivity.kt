f06a04a2c5584228ca065b1b4290d13f42f33c11
Bojan Antic <maatori90@gmail.com>
2019-05-07 13:34:09 +0200

    -- migrated project to AndroidX

--- app/src/main/java/com/example/chatapp/NewMessageActivity.kt
+++ app/src/main/java/com/example/chatapp/NewMessageActivity.kt
@@ -1,12 +1,16 @@
 package com.example.chatapp
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+        title = "Select User"
+
+//        recyclerview_newmessage.adapter
     }
 }
